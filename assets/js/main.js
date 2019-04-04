1db431aa280e9621447e9f9e4bd04f9c361e47dd
YewP <px@192.168.3.13>
2019-04-04 22:00:44 +0800

    img

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,8 +4,11 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-(function($) {
 
+(function($) {
+	var observer = lozad();
+	observer.observe();
+	
 	var $window = $(window),
 		$body = $('body'),
 		$header = $('#header'),
@@ -113,8 +116,4 @@
 				});
 
 			});
-
-			var observer = lozad();
-			observer.observe();
-
 })(jQuery);
